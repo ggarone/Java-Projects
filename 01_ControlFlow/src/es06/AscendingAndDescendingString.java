a055bc000cccd1370d50fd5507d08974efaa0e58
ggarone <96467406+ggarone@users.noreply.github.com>
2022-01-07 12:02:50 +0100

    Add files via upload

--- 01_ControlFlow/src/es06/AscendingAndDescendingString.java
+++ 01_ControlFlow/src/es06/AscendingAndDescendingString.java
@@ -3,8 +3,31 @@ package es06;
 public class AscendingAndDescendingString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		final int DIM = 6;
+		Integer counterRight = DIM;
+		Integer counterLeft = 2;
+		Integer spaces = DIM;
+		
+		
+		for (int i = 0; i < DIM; i++) {
+			for (int k = 1; k < counterLeft; k++) {
+				System.out.print(k);
+			}
+			
+			System.out.print("\t");
+			while(spaces != counterRight) {
+				System.out.print(" ");
+				spaces--;
+			}
+			
+			for (int j = counterRight; j > 0; j--) {
+				System.out.print(j);
+			}
+			
+			System.out.println();
+			counterRight--;
+			counterLeft++;
+			spaces = DIM;
+		}
 	}
-
 }
