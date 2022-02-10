f7490fc0840dd718a9c2726ca18d575fa455c4c5
Giuseppe <96467406+ggarone@users.noreply.github.com>
2022-02-10 11:54:32 +0100

    Add files via upload

---
+++ Prg29_HelloDB/src/dal/CanzoneDAO.java
@@ -0,0 +1,23 @@
+package dal;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import model.Canzone;
+
+public interface CanzoneDAO {
+	String GET_ALL = "select * from canzoni";
+	
+	//UPDATE DB
+	void addCanzone(Canzone c) throws SQLException;
+	void addCanzone(String titolo,String cantante, String genere)  throws SQLException;
+	void updCanzone(Canzone c) throws SQLException;
+	void delCanzone(int id) throws SQLException;
+	
+	//QUERY DB
+	Canzone getCanzoneById(int id) throws SQLException;
+	Canzone getCanzoneByTitolo(String titolo) throws SQLException;
+	List<Canzone> getCanzoni() throws SQLException;
+	
+	
+}
---
+++ Prg29_HelloDB/src/dal/Connessione.java
@@ -0,0 +1,30 @@
+package dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connessione {
+	private final String DB_URL = "jdbc:mysql://localhost:3306/tss_2022";
+	private final String USER = "root";
+	private final String PASS = "V3cch10n3_!";
+	
+	private Connection conn = null;
+	
+	public Connection connetti() {
+		try {
+			this.conn = DriverManager.getConnection(DB_URL,USER,PASS);
+			System.out.println("Connessione ok!");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.err.println("Errore di connessione.");
+			System.err.println(e.getMessage());
+		}
+		return this.conn;
+	}
+	
+	public static void main(String[] args) {
+		Connessione c = new Connessione();
+		c.connetti();
+	}
+}
